bc0490b6260a82b60bcce902af18a8eeb59cea65
tien_kenny <tienkenny96@gmail.com>
2022-06-24 01:42:38 -0700

    Modified Video class get and put methods to use DB queries

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, reqparse, abort
+from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy, Model
 
 app = Flask(__name__)
@@ -23,26 +23,33 @@ video_put_args.add_argument("name", type=str, help="Name of the video is require
 video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes on the video is requried", required=True)
 
-videos = {}
-
-def abort_if_video_id_doesnt_exist(video_id):
-    if video_id not in videos:
-        abort(404, message="Could not find video...")
-
-def abort_if_video_exists(video_id):
-    if video_id in videos:
-        abort(409, message="Video already exists with that ID...")
+resource_fields = {
+    'id' : fields.Integer,
+    'name' : fields.String,
+    'views' : fields.Integer,
+    'likes' : fields.Integer
+}
 
 class Video(Resource):
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        abort_if_video_id_doesnt_exist(video_id)
-        return videos[video_id]
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find video with that ID")
+        return result
 
+    @marshal_with(resource_fields)
     def put(self, video_id):
-        abort_if_video_exists(video_id)
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201
+
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="Video ID taken...")
+
+        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
 
     def delete(self, video_id):
         abort_if_video_id_doesnt_exist(video_id)
--- test.py
+++ test.py
@@ -11,9 +11,7 @@ for i in range(len(data)):
     response = requests.put(BASE + "video/" + str(i), data[i])
     print(response.json())
 
-input()
-response = requests.delete(BASE + "video/0")
-print(response)
+
 input()
 response = requests.get(BASE + "video/2")
 print(response.json())
