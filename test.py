0f77b0ec71bca6f2ea27c53ae41741c8a34e3d04
tien_kenny <tienkenny96@gmail.com>
2022-06-24 11:42:09 -0700

    Commented out data input lines

--- test.py
+++ test.py
@@ -2,16 +2,16 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-data = [{"name" : "Kenny", "views" : 1000000, "likes" : 357},
-        {"name" : "How to make REST API", "views" : 1000000, "likes" : 100075},
-        {"name" : "Best Food in NYC" , "views" : 1000000, "likes" : 357},
-        ]
+# data = [{"name" : "Kenny", "views" : 1000000, "likes" : 357},
+#         {"name" : "How to make REST API", "views" : 1000000, "likes" : 100075},
+#         {"name" : "Best Food in NYC" , "views" : 1000000, "likes" : 357},
+#         ]
+#
+# for i in range(len(data)):
+#     response = requests.put(BASE + "video/" + str(i), data[i])
+#     print(response.json())
 
-for i in range(len(data)):
-    response = requests.put(BASE + "video/" + str(i), data[i])
-    print(response.json())
 
-
-input()
-response = requests.get(BASE + "video/2")
+# input()
+response = requests.patch(BASE + "video/2", {"views" : 99, "likes" : 5})
 print(response.json())
