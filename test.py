f849838821323719b2217eff83e3bb9761bb05c4
tien_kenny <tienkenny96@gmail.com>
2022-06-24 01:27:54 -0700

    Before implementation of SQLAlchemy for storing data

---
+++ test.py
@@ -0,0 +1,19 @@
+import requests
+
+BASE = "http://127.0.0.1:5000/"
+
+data = [{"name" : "Kenny", "views" : 1000000, "likes" : 357},
+        {"name" : "How to make REST API", "views" : 1000000, "likes" : 100075},
+        {"name" : "Best Food in NYC" , "views" : 1000000, "likes" : 357},
+        ]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/" + str(i), data[i])
+    print(response.json())
+
+input()
+response = requests.delete(BASE + "video/0")
+print(response)
+input()
+response = requests.get(BASE + "video/2")
+print(response.json())
